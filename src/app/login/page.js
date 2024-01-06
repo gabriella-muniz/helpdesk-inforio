e9c61747f65051d22f8e03b6f577106e632f30dc
Gabriella Muniz <gabriellamunizzz08@gmail.com>
2024-01-06 14:14:00 -0300

    alterações

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,28 +3,34 @@ import Link from 'next/link'
 import Button from '../components/button/button';
 import Input from '../components/input/input';
 import LoginCard from "../login-card/page";
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../../services/firebase'; 
+import { useRouter } from 'next/router';
 
 
-export default function Login(){
+
+export default function Login() {
+   
     return (
         
-        <> 
+      <> 
         
-        <div className='background'> 
-        <LoginCard title= "Entre na sua conta">
-        
-        <form className='form'> 
-            <Input type= "email" placeholder="Digite seu email" />
-            <Input type= "password" placeholder="Digite sua senha" />
-    
-           <Button>Entrar</Button>
-           <Link href="/cadastro">Ainda não possui conta? Crie uma! </Link>
-        </form>
-           
-           </LoginCard>
-           </div>
-           
-           </>
-       
-    )
+      <div className='background'> 
+      <LoginCard title= "Entre na sua conta">
+      
+      <form className='form'> 
+          <Input type= "email" placeholder="Digite seu email" />
+          <Input type= "password" placeholder="Digite sua senha" />
+  
+         <Button>Entrar</Button>
+         <Link href="/cadastro">Ainda não possui conta? Crie uma! </Link>
+      </form>
+         
+         </LoginCard>
+         </div>
+         
+         </>
+     
+  )
 }
